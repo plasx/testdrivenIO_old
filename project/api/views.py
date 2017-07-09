a95a7376499630b51062375d4f719d11553295eb
plasx <plasx@live.com>
2017-07-08 23:09:05 -0400

    made all tests pass

--- project/api/views.py
+++ project/api/views.py
@@ -1,7 +1,7 @@
 # project/api/views.py
 
 from flask import Blueprint, jsonify, request, make_response
-
+from sqlalchemy import exc
 from project.api.models import User
 from project import db
 
@@ -19,8 +19,37 @@ def ping_pong():
 @users_blueprint.route('/users', methods=['POST'])
 def add_user():
     post_data = request.get_json()
+    if not post_data:
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return make_response(jsonify(response_object)), 400
     username = post_data.get('username')
     email = post_data.get('email')
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'{email} was added!'
+            }
+            return make_response(jsonify(response_object)), 201
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Sorry. That email already exists.'
+            }
+            return make_response(jsonify(response_object)), 400
+    except exc.IntegrityError as e:
+        db.session().rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return make_response(jsonify(response_object)), 400
     db.session.add(User(username=username, email=email))
     db.session.commit()
     response_object = {
